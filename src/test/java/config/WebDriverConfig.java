73a6f892ad5aeac6617c0f5b889b9fac0f4d94c9
TerraVulccano <ilya.deryushev@gmail.com>
2021-11-22 20:11:00 +0300

    added test, config and properties

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+import static org.aeonbits.owner.Config.LoadType.MERGE;
+
+@Config.LoadPolicy(MERGE)
+@Config.Sources({"system:properties",
+        "classpath:${environment}.properties"})
+public interface WebDriverConfig extends Config {
+    @DefaultValue("false")
+    @Key("webdriver.remote")
+    boolean isRemote();
+
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub/")
+    @Key("webdriver.remote.url")
+    String getRemoteURL();
+
+    @DefaultValue("CHROME")
+    @Key("webdriver.browser")
+    String getBrowser();
+
+    @DefaultValue("89.0")
+    @Key("webdriver.browser.version")
+    String webdriverBrowserVersion();
+}
---
+++ src/test/java/tests/WebDriverTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebDriverTest extends TestBase {
+
+    private final static String REPOSITORY = "eroshenkoam/allure-example";
+    private final static int ISSUE_NUMBER = 65;
+    private final String BASE_URL = "https://github.com";
+
+    @Test
+    @DisplayName("Тест с селенидовским логгером")
+    public void testIssueSearch() {
+
+        open(BASE_URL);
+        $(".header-search-input").setValue(REPOSITORY).submit();
+        $(By.linkText(REPOSITORY)).click();
+        $("#issues-tab").click();
+        $(withText("#" + ISSUE_NUMBER)).shouldBe(Condition.visible);
+    }
+}
